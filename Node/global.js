a497e1f6e6037d872a6e7432f95a5dfdebc45f71
Yab1 <yabilisanu@gmail.com>
2023-12-13 20:32:55 +0300

    Initialize Node global variables

---
+++ Node/global.js
@@ -0,0 +1,13 @@
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("first");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log("second");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
